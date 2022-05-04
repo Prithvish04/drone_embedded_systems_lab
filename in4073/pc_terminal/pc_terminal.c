a2df5c4b7edc5d1a7a36acf312c7fc4717c933a1
nembha28 <Prithvish.Nembhani.ext@imec.nl>
2022-05-04 19:44:45 +0200

    Revert "seperating files"
    This reverts commit c63308f37d71e88dab46e75ee248c1ceaf9e6f87.

--- in4073/pc_terminal/pc_terminal.c
+++ in4073/pc_terminal/pc_terminal.c
@@ -163,7 +163,30 @@ int serial_port_putchar(char c)
 	return result;
 }
 
+long long to_dec(char hex[], int length)
+{
+	long long decimal = 0, base = 1;
+	for(int i = length--; i >= 0; i--)
+	{
+		if(hex[i] >= '0' && hex[i] <= '9')
+		{
+		    decimal += (hex[i] - 48) * base;
+		    base *= 16;
+		}
+		else if(hex[i] >= 'A' && hex[i] <= 'F')
+		{
+		    decimal += (hex[i] - 55) * base;
+		    base *= 16;
+		}
+		else if(hex[i] >= 'a' && hex[i] <= 'f')
+		{
+		    decimal += (hex[i] - 87) * base;
+		    base *= 16;
+		}
+	}
+	return decimal;
 
+}
 /*----------------------------------------------------------------
  * main -- execute terminal
  *----------------------------------------------------------------
