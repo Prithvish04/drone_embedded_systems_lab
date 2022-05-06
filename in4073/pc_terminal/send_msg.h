5a52aaafa88aa3f9b0e300deffaebdc51f8a9033
nembha28 <Prithvish.Nembhani.ext@imec.nl>
2022-05-06 12:23:39 +0200

    adding sending msg header

---
+++ in4073/pc_terminal/send_msg.h
@@ -0,0 +1,27 @@
+#ifndef MESSAGE_H_
+#define MESSAGE_H_
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+struct message
+{
+	uint8_t start;
+
+	int16_t pitch;
+	int16_t roll;
+	int16_t yaw;
+    int16_t flap;
+    int8_t mode;
+	
+	uint8_t stop;	
+};
+
+void toBinary(unsigned n, int len);
+void print_message(struct message m);
+void print_message_hex(struct message m);
+
+
+#endif /* MESSAGE_H_ */
